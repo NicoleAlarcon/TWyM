9698a94dd1f9169c617e6d7e86435c32ab8e9016
Nicole Alarcón <104875439+NicoleAlarcon@users.noreply.github.com>
2023-09-15 11:18:04 -0300

    actualizacion

---
+++ JS/05.prompt.confirm.js
@@ -0,0 +1,8 @@
+//Utilizando el Prompt
+let edad = prompt('¿Cuál es tu edad?', 0); //Contiene un texto a mostrar al usuario y un valor inicial (opcional)
+alert(`Tienes ${age} años!`); //El Valor que ingreses
+
+
+//Utilizando el Confirm
+let question = confirm("¿Pasarás el ramo?"); //Modal con una pregunta y 2 botones 
+alert(question);  //Si se pulva Aceptar lanzará True, si se pulsa Cancelar lanzará False
